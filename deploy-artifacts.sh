023296a5bd2c83c597caa547a46fbf7e222e1866
Artem Sidorkin <artem.sidorkin@digitalarrowtech.com>
2020-05-28 15:05:50 +0300

    Used docker gradle plugin for publishing.

--- deploy-artifacts.sh
+++ deploy-artifacts.sh
@@ -2,25 +2,12 @@
 
 BRANCH=$(git rev-parse --abbrev-ref HEAD)
 
-VERSION=$BRANCH
+export DOCKER_IMAGE_TAG=latest
 
-DOCKER_REPO=eventuateio
-DOCKER_COMPOSE_PREFIX=eventuateexamples_
-DOCKER_REMOTE_PREFIX=eventuate-
+docker login -u ${DOCKER_USER_ID?} -p ${DOCKER_PASSWORD?}
 
-function tagAndPush() {
-  LOCAL=$1
-  REMOTE=$2
-  FULL_REMOTE=${DOCKER_REMOTE_PREFIX}${REMOTE}
-  $PREFIX docker tag ${DOCKER_COMPOSE_PREFIX?}$LOCAL $DOCKER_REPO/${FULL_REMOTE}:$VERSION
-  $PREFIX docker tag ${DOCKER_COMPOSE_PREFIX?}$LOCAL $DOCKER_REPO/${FULL_REMOTE}:latest
-  echo Pushing $DOCKER_REPO/${FULL_REMOTE}:$VERSION
-  $PREFIX docker push $DOCKER_REPO/${FULL_REMOTE}:$VERSION
-  $PREFIX docker push $DOCKER_REPO/${FULL_REMOTE}:latest
-}
+./gradlew javaexampleComposePull || echo no image to pull
 
-$PREFIX docker login -u ${DOCKER_USER_ID?} -p ${DOCKER_PASSWORD?}
+./gradlew javaexampleComposeBuild
 
-docker images
-
-tagAndPush "javaexamplebaseimage" "java-example-base-image"
+./gradlew javaexampleComposePush
