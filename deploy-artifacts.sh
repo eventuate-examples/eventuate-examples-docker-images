9f3c69cec708d56620e339c07e0de7f491eb5b40
Chris Richardson <chris@chrisrichardson.net>
2020-05-29 08:06:04 -0700

    Simplifications

--- deploy-artifacts.sh
+++ deploy-artifacts.sh
@@ -2,13 +2,17 @@
 
 BRANCH=$(git rev-parse --abbrev-ref HEAD)
 
-export DOCKER_IMAGE_TAG=latest
+if [  $BRANCH == "master" ] ; then
+  export DOCKER_IMAGE_TAG=BUILD-${CIRCLE_BUILD_NUM?}
+elif [[  $BRANCH =~ RELEASE$ ]] ; then
+  export DOCKER_IMAGE_TAG=$BRANCH
+else
+  echo cannot figure out version for this branch $BRANCH
+  exit -1
+fi
+
+docker-compose build
 
 docker login -u ${DOCKER_USER_ID?} -p ${DOCKER_PASSWORD?}
 
-docker-compose -f docker-compose-java-example-base-image.yml pull || echo no image to pull
-
-docker-compose -f docker-compose-java-example-base-image.yml build
-
-docker-compose -f docker-compose-java-example-base-image.yml push
-
+docker-compose push
