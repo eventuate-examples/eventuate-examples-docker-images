8ac381351f67d582174e0d97924a5eed1f4e4a22
Artem Sidorkin <artem.sidorkin@digitalarrowtech.com>
2020-05-29 16:30:24 +0300

    Fixed deploy-artifacts.sh

--- deploy-artifacts.sh
+++ deploy-artifacts.sh
@@ -1,27 +1,14 @@
-
-#! /bin/bash -e
+#!/bin/bash -e
 
 BRANCH=$(git rev-parse --abbrev-ref HEAD)
 
-VERSION=$BRANCH
+export DOCKER_IMAGE_TAG=latest
 
-DOCKER_REPO=eventuateio
-DOCKER_COMPOSE_PREFIX=eventuateexamples_
-DOCKER_REMOTE_PREFIX=eventuate-
+docker login -u ${DOCKER_USER_ID?} -p ${DOCKER_PASSWORD?}
 
-function tagAndPush() {
-  LOCAL=$1
-  REMOTE=$2
-  FULL_REMOTE=${DOCKER_REMOTE_PREFIX}${REMOTE}
-  $PREFIX docker tag ${DOCKER_COMPOSE_PREFIX?}$LOCAL $DOCKER_REPO/${FULL_REMOTE}:$VERSION
-  $PREFIX docker tag ${DOCKER_COMPOSE_PREFIX?}$LOCAL $DOCKER_REPO/${FULL_REMOTE}:latest
-  echo Pushing $DOCKER_REPO/${FULL_REMOTE}:$VERSION
-  $PREFIX docker push $DOCKER_REPO/${FULL_REMOTE}:$VERSION
-  $PREFIX docker push $DOCKER_REPO/${FULL_REMOTE}:latest
-}
+docker-compose -f docker-compose-java-example-base-image.yml pull || echo no image to pull
 
-$PREFIX docker login -u ${DOCKER_USER_ID?} -p ${DOCKER_PASSWORD?}
+docker-compose -f docker-compose-java-example-base-image.yml build
 
-docker images
+docker-compose -f docker-compose-java-example-base-image.yml push
 
-tagAndPush "javaexamplebaseimage" "java-example-base-image"
