3b2e8b9ae57f5c1cb542bc3dabcd683a376fd4c8
Chris Richardson <chris@chrisrichardson.net>
2025-02-08 16:22:59 +0900

    Upgrade Base Image + switch to semver format + publish version JAR

--- deploy-multi-arch-artifacts.sh
+++ deploy-multi-arch-artifacts.sh
@@ -3,15 +3,15 @@
 
 TARGET_TAG=$(./target-tag.sh)
 
-docker login -u ${DOCKER_USER_ID?} -p ${DOCKER_PASSWORD?}
+docker login -u "${DOCKER_USER_ID?}" -p "${DOCKER_PASSWORD?}"
 
 build_image() {
   framework=${1?}
   docker buildx build \
    --platform linux/amd64,linux/arm64 \
-   -t eventuateio/eventuate-examples-docker-images-${framework}-example-base-image:${TARGET_TAG} \
+   -t "eventuateio/eventuate-examples-docker-images-${framework}-example-base-image:${TARGET_TAG}" \
    --push \
-  ${framework}-example-base-image
+   "${framework}-example-base-image"
 }
 
 build_image spring
